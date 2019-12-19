590f1f730851dd6d3d2ad64a4a912451aef071d8
sage-jordan <sagemjrodan@gmail.com>
2019-12-19 01:08:48 -0500

    Now auth middleware works.................?

--- Routes/auth/auth-middleware.js
+++ Routes/auth/auth-middleware.js
@@ -1,17 +1,35 @@
 module.exports = (req, res, next) => {
-
-    if(req.session && req.session.user){
-
+    if(req.params.id){
         const {id} = req.params;
-        const userId = req.session.user.id;
 
-        if(id.toString() === userId.toString() || req.session.admin){ 
-            next();
+        if(req.session && req.session.user){
+
+            const userId = req.session.user.id;
+    
+            if(id.toString() === userId.toString() || req.session.admin){ 
+                next();
+            } else {
+                res.status(401).json({ message: 'Not logged in as Admin!'});
+            }
+    
         } else {
-            res.status(401).json({ message: 'Not logged in as Admin!'});
+            res.status(404).json({ message: "Please login"})
         }
-        
+
     } else {
-        res.status(404).json({ message: "Please login"})
-    }
+        const id = 0;
+        if(req.session && req.session.user){
+
+            const userId = req.session.user.id;
+    
+            if(id.toString() === userId.toString() || req.session.admin){ 
+                next();
+            } else {
+                res.status(401).json({ message: 'Not logged in as Admin!'});
+            }
+    
+        } else {
+            res.status(404).json({ message: "Please login"})
+        }
+    };
 };
