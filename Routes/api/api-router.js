2b4af20f94eb8fe581b53d9cbc590ff8c666b58a
sage-jordan <sagemjrodan@gmail.com>
2019-12-18 21:48:25 -0500

    server.js changes, api-router and middleware setup

---
+++ Routes/api/api-router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const authRouter = require('../auth/auth-router');
+const userRouter = require('../users/users-router');
+
+router.use('/auth', authRouter);
+router.use('/users', userRouter);
+
+router.get('/', (req, res) => {
+    res.json({ api: "It's alive" });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const server = express();
 
 const apiRouter = require('./Routes/api/api-router');
+const middleware = require('./middleware');
 
-server.use(express.json());
+middleware(server);
 
 server.use('/api', apiRouter);
 
+module.exports = server;
