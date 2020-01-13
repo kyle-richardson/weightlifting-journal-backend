ad218c64f7ed2a18d52813ee14be4e8918c8e19c
sage-jordan <sagemjrodan@gmail.com>
2020-01-12 21:18:28 -0500

    tests for /register and GET /users

--- __test__/auth.router.spec.js
+++ __test__/auth.router.spec.js
@@ -1,16 +1,13 @@
 const request = require('supertest'); //install 
-const server = require('./server.js');
-const Users = require('../Routes/users/users-model');
+const server = require('../server');
 
 describe('authentication routes', () => {
-    describe('POST to /register returns a user', () => {
-        const testuser = { 'username': 'test', 'password':'passtest', 'department': 'Student'};
-        const res = await request(server).post('/api/auth/register').send(testuser);
-        res.expect(200);
-        res.end((err, res) => {
-            if (err) done(err);
-            expect(res.saved).toEqual(expect.any(Object));
-            done();
-        });
+    it('POST to /register returns 200 with json', async () => {
+        request(server)
+        .post('/api/auth/register')
+        .send({username: "test", password: "testpass", department:"Student"})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
     });
 });
---
+++ __test__/users.router.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest'); //install 
+const server = require('../server');
+
+describe('GET /users', function() {
+    it('responds with 404 please login', function() {
+      return request(server)
+        .get('/api/users')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .expect(function(res) {
+            res.message = 'Please login'
+        });
+    });
+  });
