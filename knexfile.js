a16c3f6a97ab1f18e6764163a717e1683e073adb
Kyle Richardson <kyle.richardson1@gmail.com>
2021-02-06 12:07:22 -0600

    comment out pool in pg conn

--- knexfile.js
+++ knexfile.js
@@ -45,15 +45,15 @@ module.exports = {
     seeds: {
       directory: "./db/seeds"
     },
-    pool: {
-      min: 0,
-      max: 6,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false
-    },
+    // pool: {
+    //   min: 0,
+    //   max: 6,
+    //   createTimeoutMillis: 3000,
+    //   acquireTimeoutMillis: 30000,
+    //   idleTimeoutMillis: 30000,
+    //   reapIntervalMillis: 1000,
+    //   createRetryIntervalMillis: 100,
+    //   propagateCreateError: false
+    // },
   }
 };
